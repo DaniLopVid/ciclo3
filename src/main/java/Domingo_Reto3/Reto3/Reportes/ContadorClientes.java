d185dda43142a0f6a93e8b81a51e4df00527f2bd
DaniLopVid <92611212+DaniLopVid@users.noreply.github.com>
2021-10-30 09:07:15 -0500

    Add files via upload

---
+++ src/main/java/Domingo_Reto3/Reto3/Reportes/ContadorClientes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domingo_Reto3.Reto3.Reportes;
+
+import Domingo_Reto3.Reto3.Client;
+
+/**
+ *
+ * @author danie
+ */
+public class ContadorClientes {
+    
+    private Long total;
+    private Client client;
+
+    public ContadorClientes(Long total, Client client) {
+        this.total = total;
+        this.client = client;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+    
+    
+    
+}
